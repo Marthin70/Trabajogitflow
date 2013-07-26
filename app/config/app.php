65ea3451bc75ec1c98acbc9664f875c1c57fba72
anheru88 <ajimenez@digitalcaribe.com.co>
2013-07-25 23:16:22 -0500

    Configuracion basica de laravel

--- app/config/app.php
+++ app/config/app.php
@@ -65,7 +65,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => 'LAe8DCPqajLfF575hbdZZH8wJBmnbNfh',
 
 	/*
 	|--------------------------------------------------------------------------
